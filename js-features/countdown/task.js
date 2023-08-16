e1ab21b9aa8977f39768c4bf68ed17fe4759a764
NikaN <NikaN7602@gmail.com>
2023-08-16 23:23:39 +0200

    js-features/countdown/task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -92,3 +92,18 @@ function getTimeRemaining(endtime) {
 
 // Интересно рассмотреть эту задачу!
 
+var url = document.createElement("a");
+url.href =
+  "https://developer.mozilla.org/en-US/search?q=URL#search-results-close-container";
+console.log(url.href); // https://developer.mozilla.org/en-US/search?q=URL#search-results-close-container
+console.log(url.protocol); // https:
+console.log(url.host); // developer.mozilla.org
+console.log(url.hostname); // developer.mozilla.org
+console.log(url.port); // (пустой - https подразумевает порт 443)
+console.log(url.pathname); // /en-US/search
+console.log(url.search); // ?q=URL
+console.log(url.hash); // #search-results-close-container
+console.log(url.origin); // https://developer.mozilla.org
+
+// Такой пример
+
