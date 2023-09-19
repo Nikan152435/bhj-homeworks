57ee89342193f43bcbe72eca7229780cd22244b9
NikaN <NikaN7602@gmail.com>
2023-09-19 23:57:26 +0200

    event-object/dropdown/task.html

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,6 +1,6 @@
-const dropdowwnValue = document.querySelector('.dropdown__value');
-const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
-const dropdownList = document.querySelector('.dropdown__list');
+const dropdowwnValue = document.querySelector('dropdown__value');
+const dropdownItems = Array.from(document.querySelectorAll('dropdown__item'));
+const dropdownList = document.querySelector('dropdown__list');
 
 dropdowwnValue.addEventListener('click', function() {
   dropdownList.classList.add('dropdown__list_active');
@@ -11,5 +11,5 @@ dropdownItems.forEach(item =>
     e.preventDefault();
     dropdowwnValue.textContent = this.textContent;
     dropdownList.classList.remove('dropdown__list_active');        
-  });
+  })
 );
