de7b5a1cf1e5ec3555f9bf8d66fa79728acdf43d
NikaN <NikaN7602@gmail.com>
2023-10-12 17:47:35 +0200

    event-object/dropdown/task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,21 +1,31 @@
+
+//Разбор задачи с выпадающим и сворачивающимся списком
+
 const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
 const dropdownList = document.querySelector('.dropdown__list');
-//const closest = dropdownValue.closest(".dropdown__value");
+let isListOpen = false; // Флаг, показывающий, открыт ли список
 
 dropdownValue.addEventListener('click', function(e) {
-  dropdownList.classList.add('dropdown__list_active');
+  // Переключаем состояние флага при клике на dropdownValue
+  isListOpen = !isListOpen;
+
+  if (isListOpen) {
+    // Если флаг true, то добавляем класс 'dropdown__list_active' и показываем список
+    dropdownList.classList.add('dropdown__list_active');
+  } else {
+    // Если флаг false, то убираем класс 'dropdown__list_active' и скрываем список
+    dropdownList.classList.remove('dropdown__list_active');
+  }
 });
-
-
 
 dropdownItems.forEach(item => 
   item.addEventListener('click', function(e) {
     e.preventDefault();
     dropdownValue.textContent = this.textContent;
-    dropdownList.classList.remove('dropdown__list_active');        
+    
+    // Закрываем список при выборе элемента
+    isListOpen = false;
+    dropdownList.classList.remove('dropdown__list_active');
   })
 );
-
-
-// console.log(dropdownValue);
